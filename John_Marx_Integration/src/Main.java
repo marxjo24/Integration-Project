070911968698e0314a727c9308d83b9a16030992
jwmarx6371 <jwmarx6371@L438R4S1.STUDENT.ad.fgcu.edu>
2018-01-25 19:00:46 -0500

    changed past main to practice. Also created a new java to make a simpler int to double and double to int program.

--- John_Marx_Integration/src/Main.java
+++ John_Marx_Integration/src/Main.java
@@ -2,35 +2,17 @@ import java.util.Scanner;
 
 // John Marx
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Hello World");
-		//sysout Ctrl+Space
-		System.out.println("Enter x1");
-		int x1 = scan.nextInt();
-		System.out.println("Enter Rate");
-		double rate = scan.nextDouble();
-		System.out.println("What is your name");
-		scan.nextLine();
-		String username = scan.nextLine();
-		System.out.println(username);
-		
-		
-		Dog spot = new Dog();
-		spot.size = 30;
-		spot.breed = "German Shepherd";
-		System.out.println(spot.breed);
-		Dog spuds = new Dog();
-		spuds.size= 40;
-		
+		System.out.println("Enter Your Name");
+		String usersname = scan.next();
+		System.out.println("Hello " + usersname);
+		System.out.println("Enter a interger");
+		int num1 = scan.nextInt();
+		System.out.println((double)num1 + " Is Now A Double");
+		System.out.println("Enter A Double");
+		double num2 = scan.nextDouble();
+		System.out.println((int)num2 + " Is Now A Double");
 	}
-
-}
-
-
-class Dog{
-	double size;
-	String breed;
+	
 }
