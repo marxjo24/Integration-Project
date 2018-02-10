bf9e60efece341edad9f840a1e5cb3bf52acecf2
Marxj <Marxj@172.26.142.212>
2018-02-10 15:39:22 -0500

    Final commit for first sprint

--- John_Marx_Integration/src/Main.java
+++ John_Marx_Integration/src/Main.java
@@ -7,16 +7,28 @@ public class Main {
     System.out.println("Enter Your Name");
     String usersname = scan.next();
     System.out.println("Hello " + usersname);
+    System.out.println();
     
     System.out.println("Enter a interger");
-    int num1 = scan.nextInt();
-    System.out.println(num1 + " Is Now A Double");
-    System.out.println((double)num1);
+    double num1 = scan.nextDouble();
+    System.out.println((int)num1 + " Is Now A Double");
+    System.out.println(num1);
+    System.out.println();
     
     System.out.println("Enter A Double");
     double num2 = scan.nextDouble();
     System.out.println(num2 + " Is Now A Interger");
     System.out.println((int)num2);
+    System.out.println();
+    
+    System.out.println("Enter another interger to be divided by the previous "
+        + "number");
+    double num4 = scan.nextDouble();
+    System.out.println((int)num4 + " divided by " + (int)num2 + " is: " + (int)num4/(int)num2);
+    System.out.println();
+    System.out.println("The decimal was removed due to it being an interger "
+        + "division");
+    System.out.println();
     
     System.out.println("Enter a interger between 1 and 20");
     double num3 = scan.nextDouble();
@@ -34,6 +46,7 @@ public class Main {
     } else {
       System.out.println("That is not a number between 1 and 20");
     }
+    System.out.println();
     
     System.out.println("Enter a grade you recieved on a test");
     scan.nextLine();
@@ -61,8 +74,10 @@ public class Main {
       }
     //Uses the CalculateVolume class to get the volume from the values that the
     //user gives
+    System.out.println();
     System.out.println("The program will now calculate volume using a seperate"
         + " class.");
+    System.out.println();
     System.out.println("Enter the width for a box");
     CalculateVolume volume1 = new CalculateVolume();
     volume1.getWidth(scan.nextDouble());  //Gets the width
@@ -70,6 +85,7 @@ public class Main {
     volume1.getHeight(scan.nextDouble());  //Gets the height
     System.out.println("Enter the lenght for the box");
     volume1.getLength(scan.nextDouble());  //Gets the length
+    System.out.println();
     volume1.getVolume();  //Calculates the volume in separate class and 
                           //displays it
     }
