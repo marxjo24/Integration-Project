b544446450670c1ad45e02c74b769601fa863cb0
John Marx <Marxj@172.26.142.212>
2018-02-10 04:39:03 -0500

    Almost finished with first sprint. Need commits and go over requirements

---
+++ John_Marx_Integration/src/CalculateVolume.java
@@ -0,0 +1,23 @@
+
+public class CalculateVolume {
+  
+  private double width;
+  private double height;
+  private double length;
+  private double volume;
+  
+  public void getWidth(double wid) {
+    width = wid;
+  }
+  public void getHeight(double hei) {
+    height = hei;
+  }
+  public void getLength(double leng) {
+    length = leng;
+  }
+  public void getVolume() {
+    volume = width*height*length;
+    System.out.println("The volume is " + volume);
+  }
+ 
+}
--- John_Marx_Integration/src/Main.java
+++ John_Marx_Integration/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 // John Marx
@@ -8,16 +7,22 @@ public class Main {
     System.out.println("Enter Your Name");
     String usersname = scan.next();
     System.out.println("Hello " + usersname);
+    
     System.out.println("Enter a interger");
     int num1 = scan.nextInt();
-    System.out.println((double) num1 + " Is Now A Double");
+    System.out.println(num1 + " Is Now A Double");
+    System.out.println((double)num1);
+    
     System.out.println("Enter A Double");
     double num2 = scan.nextDouble();
-    System.out.println((int) num2 + " Is Now A Interger");
+    System.out.println(num2 + " Is Now A Interger");
+    System.out.println((int)num2);
+    
     System.out.println("Enter a interger between 1 and 20");
     double num3 = scan.nextDouble();
     num3 = (int) num3; // If user enters a double then this changes it to a int
-    if (num3 >= 1 && num3 <= 20) {
+    
+    if (num3 >= 1 && num3 <= 20) {  
       int counter = 1;
       System.out.println("The number " + (int) num3 + " is a product of:");
       while (counter < 21) {
@@ -29,29 +34,44 @@ public class Main {
     } else {
       System.out.println("That is not a number between 1 and 20");
     }
+    
     System.out.println("Enter a grade you recieved on a test");
     scan.nextLine();
     String grade = scan.nextLine();
+    //The switch gives the user an output based on the grade they recieved
     switch (grade) {
       case "A":
         System.out.println("Good Job");
         break;
       case "B":
-        System.out.println("Get better");
+        System.out.println("Your Above Average");
         break;
       case "C":
-        System.out.println("Your average");
+        System.out.println("Your Average");
         break;
       case "D":
-        System.out.println("Your below average");
+        System.out.println("Your Below Average");
         break;
       case "F":
         System.out.println("Good Job! You failed");
         break;
       default:
-        System.out.println("Enter a letter grade");
+        System.out.println("Enter a Valid Letter Grade");
         break;
       }
+    //Uses the CalculateVolume class to get the volume from the values that the
+    //user gives
+    System.out.println("The program will now calculate volume using a seperate"
+        + " class.");
+    System.out.println("Enter the width for a box");
+    CalculateVolume volume1 = new CalculateVolume();
+    volume1.getWidth(scan.nextDouble());  //Gets the width
+    System.out.println("Enter the width for the box");
+    volume1.getHeight(scan.nextDouble());  //Gets the height
+    System.out.println("Enter the lenght for the box");
+    volume1.getLength(scan.nextDouble());  //Gets the length
+    volume1.getVolume();  //Calculates the volume in separate class and 
+                          //displays it
     }
   }
 
