ba703036fa442c6974a1f395604a7a67a39f06ee
marxjo24 <35510322+marxjo24@users.noreply.github.com>
2018-03-17 16:54:50 -0400

    Final commit for INT 2

--- John_Marx_Integration/src/Bank.java
+++ John_Marx_Integration/src/Bank.java
@@ -1,25 +1,26 @@
+//John Marx
 public class Bank {
 	private double bankBalance;
 	private static double interest;
 	
 	public Bank() {
-		bankBalance = 0;
+		bankBalance = 0.00;
 	}
 	public Bank(double mon) {
 		if (mon >=0) {
-			bankBalance = mon;
+			bankBalance = Math.round(mon*100.0)/100.0;
 		}
 		else {
-			bankBalance = 0;
+			bankBalance = 0.00;
 		}
 	}
 	
 	public void setBankBalance(double mon) {
 		if (mon >=0) {
-			bankBalance = mon;
+			bankBalance = Math.round(mon*100.0)/100.0;
 		}
 		else {
-			bankBalance = 0;
+			bankBalance = 0.00;
 		}
 	}
 	
@@ -42,14 +43,22 @@ public class Bank {
 	
 	public void makeWithdraw(double wmon) {
 		if (wmon <= bankBalance) {
-			bankBalance -= wmon;
+			bankBalance -= Math.round(wmon*100.0)/100.0;
+			System.out.println("Your current bank balance is: $" + bankBalance);
 		}
 		else {
 			System.out.println("You don't have enough money");
 		}
 	}
 	public void makeDeposit(double dmon) {
-		bankBalance += dmon;
+		bankBalance += Math.round(dmon *100.0)/100.0;
 		System.out.println("Your new bank balance is: $" + bankBalance);
+		
+	}
+	
+	public double getInterestOvertime(double prin, double rat, int numc, int yers) {
+		double newmoney = prin*Math.pow(1+rat/numc,numc*yers);
+		double rnewmoney = Math.round(newmoney * 100.0)/100.0;
+		return rnewmoney;
 	}
 }
