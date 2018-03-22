b3e990fe8f7af9ce29d0d81711049f6b764f37cd
marxjo24 <35510322+marxjo24@users.noreply.github.com>
2018-03-22 18:54:38 -0400

    Update CalculateVolume.java

--- John_Marx_Integration/src/CalculateVolume.java
+++ John_Marx_Integration/src/CalculateVolume.java
@@ -1,24 +1,27 @@
 //John Marx
 public class CalculateVolume {
-  
-  private double width;  
-  private double height;
-  private double length;
-  private double volume;
-  
-  public void getWidth(double wid) {   //This is a header
-    width = wid;
-  }
-  public void getHeight(double hei) {  //The parameter is hei
-    height = hei;
-  }
-  public void getLength(double leng) {
-    length = leng;
-  }
-  public void getVolume() {
-    volume = width*height*length;
-    System.out.println("The volume is " + volume); //System.out.println is the call
-    //("The volume is " + volume) is the argument
-  }
- 
+
+	private double width;
+	private double height;
+	private double length;
+	private double volume;
+
+	public void getWidth(double wid) { // This is a header
+		width = wid;
+	}
+
+	public void getHeight(double hei) { // The parameter is hei
+		height = hei;
+	}
+
+	public void getLength(double leng) {
+		length = leng;
+	}
+
+	public void getVolume() {
+		volume = width * height * length;
+		System.out.println("The volume is " + volume); // System.out.println is the call
+		// ("The volume is " + volume) is the argument
+	}
+
 }
