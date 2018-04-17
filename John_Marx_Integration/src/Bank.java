2247869e33f9bdaea8ea4bbf2c5f0bb982ecfb1d
marxjo24 <35510322+marxjo24@users.noreply.github.com>
2018-04-17 18:48:44 -0400

    Update Bank.java

--- John_Marx_Integration/src/Bank.java
+++ John_Marx_Integration/src/Bank.java
@@ -1,62 +1,62 @@
-//John Marx
+// John Marx
 public class Bank {
-	private double bankBalance;
-	private static double interest;
+  private double bankBalance;
+  private static double interest;
 
-	public Bank() {
-		bankBalance = 0.00;
-	}
+  public Bank() {
+    bankBalance = 0.00;
+  }
 
-	public Bank(double mon) {
-		if (mon >= 0) {
-			bankBalance = Math.round(mon * 100.0) / 100.0;
-		} else {
-			bankBalance = 0.00;
-		}
-	}
+  public Bank(double mon) {
+    if (mon >= 0) {
+      bankBalance = Math.round(mon * 100.0) / 100.0;
+    } else {
+      bankBalance = 0.00;
+    }
+  }
 
-	public void setBankBalance(double mon) {
-		if (mon >= 0) {
-			bankBalance = Math.round(mon * 100.0) / 100.0;
-		} else {
-			bankBalance = 0.00;
-		}
-	}
+  public void setBankBalance(double mon) {
+    if (mon >= 0) {
+      bankBalance = Math.round(mon * 100.0) / 100.0;
+    } else {
+      bankBalance = 0.00;
+    }
+  }
 
-	public static void setInterest(double inter) {
-		if (inter > 0) {
-			interest = inter;
-		} else {
-			interest = 0.01;
-		}
-	}
+  public static void setInterest(double inter) {
+    if (inter > 0) {
+      interest = inter;
+    } else {
+      interest = 0.01;
+    }
+  }
 
-	public double getBankBalance() {
-		return bankBalance;
-	}
+  public double getBankBalance() {
+    return bankBalance;
+  }
 
-	public static double getInterest() {
-		return interest;
-	}
+  public static double getInterest() {
+    return interest;
+  }
 
-	public void makeWithdraw(double wmon) {
-		if (wmon <= bankBalance) {
-			bankBalance -= Math.round(wmon * 100.0) / 100.0;
-			System.out.println("Your current bank balance is: $" + bankBalance);
-		} else {
-			System.out.println("You don't have enough money");
-		}
-	}
+  public void makeWithdraw(double wmon) {
+    if (wmon <= bankBalance) {
+      bankBalance -= Math.round(wmon * 100.0) / 100.0;
+      System.out.println("Your current bank balance is: $" + bankBalance);
+    } else {
+      System.out.println("You don't have enough money");
+    }
+  }
 
-	public void makeDeposit(double dmon) {
-		bankBalance += Math.round(dmon * 100.0) / 100.0;
-		System.out.println("Your new bank balance is: $" + bankBalance);
+  public void makeDeposit(double dmon) {
+    bankBalance += Math.round(dmon * 100.0) / 100.0;
+    System.out.println("Your new bank balance is: $" + bankBalance);
 
-	}
+  }
 
-	public double getInterestOvertime(double prin, double rat, int numc, int yers) {
-		double newmoney = prin * Math.pow(1 + rat / numc, numc * yers);
-		double rnewmoney = Math.round(newmoney * 100.0) / 100.0;
-		return rnewmoney;
-	}
+  public double getInterestOvertime(double prin, double rat, int numc, int yers) {
+    double newmoney = prin * Math.pow(1 + rat / numc, numc * yers);
+    double rnewmoney = Math.round(newmoney * 100.0) / 100.0;
+    return rnewmoney;
+  }
 }
