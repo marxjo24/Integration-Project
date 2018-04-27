614d79368187b94c0a99c4cda8f4f48cb3efc9d0
marxjo24 <35510322+marxjo24@users.noreply.github.com>
2018-04-27 04:38:16 -0400

    Update GoodInt.java

--- John_Marx_Integration/src/GoodInt.java
+++ John_Marx_Integration/src/GoodInt.java
@@ -1,3 +1,9 @@
+//John Marx
+/** This contains the code to check if the entered integer is non-crashable.
+ * @author John Marx
+ *
+ */
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
