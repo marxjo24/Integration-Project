47e12b845e197a884894f882aeb454b2b5f7ed5b
marxjo24 <35510322+marxjo24@users.noreply.github.com>
2018-04-27 04:38:44 -0400

    Update Main.java

--- John_Marx_Integration/src/Main.java
+++ John_Marx_Integration/src/Main.java
@@ -1,17 +1,23 @@
-import java.time.LocalDate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
-import java.util.*;
+import java.time.format.*;
+import java.util.Date;
+import java.util.Scanner;
+
 
 // John Marx
-/** This is the main class where most of the code takes place
- * @author Marxj
+/**
+ * This is the main class where most of the code takes place.
+ * 
+ * @author John Marx
  *
  */
 public class Main {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    /*
+
     System.out.println("Enter Your Name");
     String usersname = scan.nextLine();
     System.out.println("Hello " + usersname);
@@ -29,16 +35,21 @@ public class Main {
     System.out.println((int) num2);
     System.out.println();
 
-    System.out.println("Enter another interger to be divided by the previous " + "number");
+    System.out.println("The difference between " + (int) num2 + " and " + num1 + " is: "
+        + Math.abs((int) num2 - num1));
+    System.out.println(num1 + " plus " + (int) num2 + " is: " + (num1 + (int) num2));
+    System.out.println(num1 + " times " + num2 + " is: " + num1 * num2);
+
+    System.out.println("Enter an interger to be divided by " + num2);
     int num4 = GoodInt.getGoodInt();
     System.out
         .println((int) num4 + " divided by " + (int) num2 + " is: " + (int) num4 / (int) num2);
     System.out.println();
     System.out.println("The decimal was removed due to it being an interger " + "division");
+    System.out.println("The remainder of " + num4 + " divided by " + num2 + " is: " + num4 % num2);
     System.out.println();
 
     System.out.println("Enter a sentence");
-    scan.nextLine();
     String sentence1 = scan.nextLine();
     System.out.println(sentence1 + " is now upper cased: ");
     System.out.println(sentence1.toUpperCase());
@@ -49,6 +60,13 @@ public class Main {
     System.out.println("I removed the first 4 characters of the sentence using " + "substing: ");
     System.out.println(sentence1.substring(4));
     System.out.println();
+    System.out.println("Using the trim method, the following string will have the leading "
+        + "and trailing spaces removed");
+    String sentence2 = "                    Lots of tabs                ";
+    System.out.println(sentence2);
+    System.out.println(sentence2.trim());
+    System.out.println();
+
     System.out.println("Enter a interger between 1 and 20");
     double num3 = GoodDouble.getGoodDouble();
     num3 = (int) num3; // If user enters a double then this changes it to a int
@@ -68,7 +86,6 @@ public class Main {
     System.out.println();
 
     System.out.println("Enter a grade you recieved on a test");
-    scan.nextLine();
     String grade = scan.nextLine();
     // The switch gives the user an output based on the grade they recieved
     switch (grade) {
@@ -97,12 +114,12 @@ public class Main {
     System.out.println("The program will now calculate volume using a seperate" + " class.");
     System.out.println();
     System.out.println("Enter the width for a box");
-    CalculateVolume volume1 = new CalculateVolume();
-    volume1.getWidth(GoodDouble.getGoodDouble()); // Gets the width
+    Volume volume1 = new Volume();
+    volume1.setWidth(GoodDouble.getGoodDouble()); // sets the width
     System.out.println("Enter the width for the box");
-    volume1.getHeight(GoodDouble.getGoodDouble()); // Gets the height
+    volume1.setHeight(GoodDouble.getGoodDouble()); // sets the height
     System.out.println("Enter the length for the box");
-    volume1.getLength(GoodDouble.getGoodDouble()); // Gets the length
+    volume1.setLength(GoodDouble.getGoodDouble()); // sets the length
     System.out.println();
     volume1.getVolume(); // Calculates the volume in separate class and
     // displays it
@@ -110,19 +127,21 @@ public class Main {
     System.out.println("The program will now emulate a bank");
     System.out.println("NOTE: the bank balance can not become negative");
     System.out.println("How much money do you want in this bank?");
+    double startMoney = GoodDouble.getGoodDouble();
     System.out.println(
-        "What is the interest at the bank? Enter the interest" + "as a decimal not a percent");
-    Bank.setInterest(GoodDouble.getGoodDouble());
+        "What is your interest at the bank? Enter the interest as a decimal not a percent");
+    Bank fakeBank1 = new Bank(startMoney);
+    fakeBank1.setInterest(GoodDouble.getGoodDouble());
     System.out.println("It is time to make a withdraw. Enter an amount to withdraw");
-    Bank fakeBank1 = new Bank(GoodDouble.getGoodDouble());
     fakeBank1.makeWithdraw(GoodDouble.getGoodDouble());
     System.out.println("You can make a deposit now. Enter a number to deposit");
     fakeBank1.makeDeposit(GoodDouble.getGoodDouble());
     System.out.println("Now we will find the compound interest over time");
     System.out.println("Enter the number of periods as a whole number,"
         + " then enter the number of years as a whole number");
-    System.out.println("The coumpound interest is: $" + fakeBank1.getInterestOvertime(
-        fakeBank1.getBankBalance(), Bank.getInterest(), GoodInt.getGoodInt(), GoodInt.getGoodInt()));
+    System.out.println(
+        "The coumpound interest is: $" + fakeBank1.getInterestOvertime(fakeBank1.getBankBalance(),
+            fakeBank1.getInterest(), GoodInt.getGoodInt(), GoodInt.getGoodInt()));
 
     System.out.println("Enter 5 integers to be placed into an array");
     int s = 0;
@@ -166,7 +185,6 @@ public class Main {
     System.out.println();
     System.out.println("Enter a string");
     StringBuilder experiment = new StringBuilder();
-    scan.nextLine();
     experiment.append(scan.nextLine());
     System.out.println("Now enter a string to be entered into the old string using append");
     String insertSomeString = scan.nextLine();
@@ -184,29 +202,61 @@ public class Main {
     int starting = GoodInt.getGoodInt();
     System.out.println(
         "Now enter string to be interted into" + " the old string at the point " + starting);
-    scan.nextLine();
     String someString = scan.nextLine();
     experiment.insert(starting, someString);
     System.out.println("The new string is:");
     System.out.println(experiment);
-    */
+    // SoccerMom is a subclass of customer,allowing someone to create a Customer object while
+    // keeping
+    // it's methods from the SoccerMom class
     System.out.println("\nThe program will now demostrate polymorphism using a restaurant class.");
     Restaurant restaurant1 = new Restaurant("McDonald's");
     Employee employee1 = new Employee("Dave", 20000);
-    Customer customer1 = new Customer("Ryan", 5);
-    SoccerMom soccerMom1 = new SoccerMom("Sherri", 0.05);
-    System.out.println("The restuarant name is " + Restaurant.returnName(restaurant1));
+    Customer customer1 = new Customer("Ryan", 5); //
+    Customer customer2 = new SoccerMom("Sherri", 0.05); // This is polymorphism
+    System.out.println("The 1st restuarant name is " + Restaurant.returnName(restaurant1));
     System.out.println("One of the employee's name is " + Restaurant.returnName(employee1));
     System.out.println("One of the customer's name is " + Restaurant.returnName(customer1));
-    System.out.println("Another one of the customer's name is " 
-        + Restaurant.returnName(soccerMom1));
-    
-    LocalDate currentDate = LocalDate.now();
+    System.out.println(
+        "Another one of the customer's name is " + Restaurant.returnName(customer2) + "\n");
+    System.out.println("Ryan's dialauge is:  " + customer1.speak());
+    System.out.println("Sherri's dialauge is:  " + customer2.speak());
+
+
+    DateFormat dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
+    Date date = new Date();
+    System.out.println("Today is: " + dateFormat.format(date));
     LocalTime currentTime = LocalTime.now();
-    System.out.println("The current date is: " + currentDate);
-    System.out.println("The current time is: " + currentTime);
-    LocalDate.parse("MM-dd-yyyy");
-    System.out.println("The current date is: " + currentDate);
+    DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm:ss a");
+    System.out.println("The time is: " + timeFormat.format(currentTime));
+    System.out.println();
+
+    // Lambda Expression
+    // Allows for a simpler version of a argument to a method
+    // The example bellow allows the programmer to use a math operation
+    // after specifying the operation he/she wants to use and the two numbers
+
+    Main tester = new Main();
+    MathOperation addition = (int a, int b) -> a + b;
+    MathOperation subtraction = (a, b) -> a - b;
+    MathOperation multiplication = (int a, int b) -> {
+      return a * b;
+    };
+    MathOperation division = (int a, int b) -> a / b;
+
+    System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+    System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+    System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+    System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+  }
+
+  interface MathOperation {
+    int operation(int a, int b);
+  }
+
+  private int operate(int a, int b, MathOperation mathOperation) {
+    return mathOperation.operation(a, b);
   }
 }
+
 
